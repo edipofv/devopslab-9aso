11fc2e43bd40601df6078388a2a6627727ada4e7
Gaby Dias <36906363+gabydias@users.noreply.github.com>
2023-03-16 22:46:41 -0300

    Desafios de Sec

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Muito bom estar com vocês!"
-
-if __name__ == '__main__':
-    app.run()
+    return "Laboratório Pipeline DevOps"
